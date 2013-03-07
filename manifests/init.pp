fbee40868ffb5e85a850b695afab19e962333cce
Geoffrey Wagner <gwagner@neufocus.com>
2013-03-07 16:12:27 -0600

    Initial Commit

---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+class htop
+{
+    include repo_epel
+
+    package {
+        #packages require a repo
+        'htop':
+            ensure => 'installed',
+            provider => 'yum',
+            require => Yumrepo['epel'];
+    }
+
+    file {
+        'htop-config-path':
+            mode => 700,
+            owner => "root",
+            group => "root",
+            path => "/root/.config/htop/",
+            recurse => true,
+            ensure => 'directory';
+
+        'htop-config':
+            mode => 664,
+            owner => "root",
+            group => "root",
+            path => "/root/.config/htop/htoprc",
+            source => "puppet:///modules/htop/htop-config",
+            require => [
+                Package["htop"],
+                File['htop-config-path']
+            ];
+    }
+}
